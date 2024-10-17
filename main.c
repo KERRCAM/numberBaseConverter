607f8984d5ad7fba4f6c645dc62886e55e1c526e
KERRCAM <kerrcameron@hotmail.com>
2024-10-17 11:30:05 +0100

    setup

--- main.c
+++ main.c
@@ -1,17 +1,67 @@
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
+#include <ctype.h>
+
+// ------------------------------------------------------------------------------------------ //
+
+int intInput(char prompt[]){
+
+    int input;
+    printf(prompt);
+    scanf("%d", &input);
+    return input;
+
+}
+
+// ------------------------------------------------------------------------------------------ //
+
+const char* stringInput(char prompt[]){
+
+    char input[] = "";
+    printf(prompt);
+    scanf("%s", &input);
+    return input;
+
+}
+
+// ------------------------------------------------------------------------------------------ //
+/*
+const char* initialNumberInput(int base){
+
+    char input[] = "";
+    printf("Enter a base %d number: ", base);
+    scanf("%s", &input);
+    return input;
+
+}
+*/
+// ------------------------------------------------------------------------------------------ //
+
+int toBase10(char initialNumber[], int base){
+
+    int base10Number = 0;
+    for (int i = 0; i < strlen(initialNumber); i++){
+        if (isalpha(initialNumber[i])){
+            base10Number += pow(base, i) * (initialNumber[i] - 55);
+        } else{
+            base10Number += pow(base, i) * (initialNumber[i] - 48);
+        }
+    }
+    return base10Number;
+
+}
+
+// ------------------------------------------------------------------------------------------ //
 
 int main(){
 
-    int base;
-    printf("Type the base of the initial number: ");
-    scanf("%d", &base);
-    char initialNumber[] = "";
-    printf("Enter a base %d number: ", base);
-    scanf("%s", &initialNumber);
+    int base = intInput("Type the base of the initial number: ");
+    //const char* initialNumber = initialNumberInput(base);
+    int base10Number = toBase10("AA", base);
+    printf("%d\n", base10Number);
 
     return 0;
 }
 
-
-
+// ------------------------------------------------------------------------------------------ //
---
+++ main.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <ctype.h>
+
+int intInput(char prompt[]);
+
+const char* stringInput(char prompt[]);
+
+int toBase10(char initialNumber[], int base);
+
+int main();
